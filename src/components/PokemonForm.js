8ac9e62a204f99f53dfe5355be3f1277398e4ab5
dyson1602 <cclark000@citymail.cuny.edu>
2020-12-16 18:50:02 -0500

    all functionality working. need to learn how to submit nested state attributes rather than spreading them to the same level then recombining them

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -2,16 +2,36 @@ import React from 'react'
 import { Form } from 'semantic-ui-react'
 
 class PokemonForm extends React.Component {
+
+  state = {
+    name: "",
+    hp: 0,
+    front: "",
+    back: ""
+  }
+
+  inputChangeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  localSubmitHandler = (e) => {
+    e.preventDefault()
+    this.props.submitHandler(this.state)
+    this.setState({ name: "", hp: 0, front: "", back: "" })
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={() => {console.log("submitting form...")}}>
+        <Form onSubmit={this.localSubmitHandler}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={this.inputChangeHandler} />
+            <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={this.inputChangeHandler} />
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="front" onChange={this.inputChangeHandler} />
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="back" onChange={this.inputChangeHandler} />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -6,10 +6,33 @@ import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
   //STATE
-  state = { pokemonArray: [] }
+  state = {
+    pokemonArray: [],
+    searchValue: ""
+  }
 
   componentDidMount() {
     this.fetchAllPokemon()
+  }
+
+  //Event Handlers
+
+  searchHandler = (e) => {
+    this.setState({ searchValue: e.target.value })
+  }
+
+  submitHandler = (pokemonObj) => {
+    let array = [...this.state.pokemonArray]
+    let newPokemon = {
+      name: pokemonObj.name,
+      hp: pokemonObj.hp,
+      sprites: {
+        front: pokemonObj.front,
+        back: pokemonObj.back
+      }
+    }
+    this.setState({pokemonArray: [newPokemon, ...array]})
+    this.postPokemonFetch(newPokemon)
   }
 
   //API REQUESTS
@@ -20,17 +43,33 @@ class PokemonPage extends React.Component {
     this.setState({ pokemonArray })
   }
 
+  postPokemonFetch = async (newPokemon) => {
+    await fetch('http://localhost:4000/pokemon', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newPokemon)
+    })
+  }
+
+  //Helper Functions
+
+  filterPokemonArray = () => {
+    return this.state.pokemonArray.filter(pokemon => pokemon.name.toLowerCase()
+      .includes(this.state.searchValue.toLowerCase()))    
+  }
+
   render() {
-    console.log(this.state.pokemonArray)
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm submitHandler={this.submitHandler}/>
         <br />
-        <Search />
+        <Search searchHandler={this.searchHandler} searchValue={this.state.searchValue} />
         <br />
-        <PokemonCollection pokemonArray={this.state.pokemonArray}/>
+        <PokemonCollection pokemonArray={this.filterPokemonArray()} />
       </Container>
     )
   }
